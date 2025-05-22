11a7ef343a134bdb32244424d6f73fff34f2b82c
Mikle Kuzin <kaktotak00p@gmail.com>
2025-05-22 14:39:26 +0200

    fixes

--- src/lib/components/mobile/WhoRWeMobile.svelte
+++ src/lib/components/mobile/WhoRWeMobile.svelte
@@ -1,23 +1,23 @@
 <div class="w-[100%] h-[1000px] relative bg-black overflow-hidden mx-auto" id="who">
     <div class=" w-[100%] h-[924px] relative">
         <div class="left-[15px] top-0 absolute text-white text-3xl font-normal font-['Craftwork_Grotesk'] uppercase">//:ХТО МИ</div>
-        <div class="w-full px-[15px] relative inline-flex flex-col justify-start items-start gap-10 mt-12">
+        <div class="w-full px-[15px] relative inline-flex flex-col justify-start items-start gap-10 mt-20">
             <div class="self-stretch flex flex-col justify-start items-start gap-[5px]">
-                <div class="self-stretch justify-start text-white text-2xl font-bold font-['Craftwork_Grotesk'] uppercase">TraffHub – це компанія</div>
+                <div class="self-stretch justify-start text-white text-2xl font-bold font-['Craftwork_Grotesk'] uppercase text-nowrap text-justify">TraffHub – це компанія</div>
                 <div class="w-full inline-flex justify-between items-center">
-                    <div class="justify-center text-white text-xl font-bold font-['Craftwork_Grotesk'] uppercase text-nowrap">у сфери iGaming</div>
-                    <img class="image w-36 h-6" src="/images/42341b6c21159b95f9718064ce2b1fd985fc2b29.png" />
+                    <div class="justify-center text-white text-[22px] font-bold font-['Craftwork_Grotesk'] uppercase text-nowrap">у сфери iGaming</div>
+                    <img class="image w-[150px] h-[26px]" src="/images/advantages1.png" />
                 </div>
             </div>
             <div class="self-stretch flex flex-col justify-start items-start gap-[3px]">
-                <div class="self-stretch justify-start text-white text-2xl font-semibold font-['Craftwork_Grotesk'] uppercase text-nowrap">З нами ти можеш вийти за</div>
+                <div class="self-stretch justify-start text-white text-[22px] font-semibold font-['Craftwork_Grotesk'] uppercase text-nowrap">З нами ти можеш вийти за</div>
                 <div class="self-stretch inline-flex justify-between items-center">
                     <img class="image w-32 h-6" src="/images/advantages2.png" />
-                    <div class="text-right justify-start text-white text-xl font-semibold font-['Craftwork_Grotesk'] uppercase text-nowrap">межі стандартів</div>
+                    <div class="text-right justify-start text-white text-[22px] font-semibold font-['Craftwork_Grotesk'] uppercase text-nowrap">межі стандартів</div>
                 </div>
             </div>
             <div class="self-stretch flex flex-col justify-start items-start gap-[5px]">
-                <div class="self-stretch justify-start text-white text-2xl font-semibold font-['Craftwork_Grotesk'] uppercase">підкорити нові вершини</div>
+                <div class="self-stretch justify-start text-white text-[25px] font-semibold font-['Craftwork_Grotesk'] uppercase">підкорити нові вершини</div>
                 <img class="image w-full h-10" src="/images/advantages3.png" />
             </div>
             <div class="self-stretch justify-start text-white text-xl font-semibold font-['Craftwork_Grotesk'] uppercase"> і працювати з командою, <br>яка надихає результатами</div>
