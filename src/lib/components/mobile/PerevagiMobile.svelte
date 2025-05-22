09e4b64d2f3a61c20c73db46909e926ca89cb47e
Mikle Kuzin <kaktotak00p@gmail.com>
2025-05-22 14:17:33 +0200

    fixes

--- src/lib/components/mobile/PerevagiMobile.svelte
+++ src/lib/components/mobile/PerevagiMobile.svelte
@@ -6,42 +6,42 @@
                 <div class="w-64 h-44 left-0 top-[10px] absolute bg-lime-400 rounded-md"></div>
                 <div class="w-60 h-40 left-[5px] top-[15px] absolute rounded-md border border-lime-600"></div>
                 <div class="w-56 left-[18px] top-[117px] absolute justify-start text-black text-xl font-bold font-['Craftwork_Grotesk'] uppercase">приватних партнерств</div>
-                <div class="left-[18px] top-[25px] absolute justify-start text-black text-6xl font-bold font-['Craftwork_Grotesk'] uppercase">10+</div>
+                <div class="left-[18px] top-[5px] absolute justify-start text-black text-[55px] font-bold font-['Craftwork_Grotesk'] uppercase">10+</div>
                 <img class="w-20 h-20 left-[188px] top-0 absolute rounded" src="/images/Vector 1.svg"/>
             </div>
             <div class="w-72 h-48 relative">
                 <div class="w-64 h-44 left-0 top-[10px] absolute bg-lime-400 rounded-md"></div>
                 <div class="w-60 h-40 left-[5px] top-[15px] absolute rounded-md border border-lime-600"></div>
-                <div class="w-56 left-[18px] top-[117px] absolute justify-start text-black text-xl font-bold font-['Craftwork_Grotesk'] uppercase">приватних партнерств</div>
-                <div class="left-[18px] top-[25px] absolute justify-start text-black text-6xl font-bold font-['Craftwork_Grotesk'] uppercase">10+</div>
+                <div class="w-56 left-[18px] top-[117px] absolute justify-start text-black text-xl font-bold font-['Craftwork_Grotesk'] uppercase">НА СТВОРЕННЯ КРЕАТИВУ</div>
+                <div class="left-[18px] top-[5px] absolute justify-start text-black text-[25px] font-bold font-['Craftwork_Grotesk'] uppercase"><span class="text-[55px]">3</span> години</div>
                 <img class="w-20 h-20 left-[188px] top-0 absolute rounded" src="/images/Vector 1.svg"/>
             </div>
             <div class="w-72 h-48 relative">
                 <div class="w-64 h-44 left-0 top-[10px] absolute bg-lime-400 rounded-md"></div>
                 <div class="w-60 h-40 left-[5px] top-[15px] absolute rounded-md border border-lime-600"></div>
-                <div class="w-56 left-[18px] top-[117px] absolute justify-start text-black text-xl font-bold font-['Craftwork_Grotesk'] uppercase">приватних партнерств</div>
-                <div class="left-[18px] top-[25px] absolute justify-start text-black text-6xl font-bold font-['Craftwork_Grotesk'] uppercase">10+</div>
+                <div class="w-56 left-[18px] top-[117px] absolute justify-start text-black text-xl font-bold font-['Craftwork_Grotesk'] uppercase">ЗРІЗІВ ВІД РЕКЛАМОДАВЦІВ</div>
+                <div class="left-[18px] top-[5px] absolute justify-start text-black text-[55px] font-bold font-['Craftwork_Grotesk'] uppercase">2%</div>
                 <img class="w-20 h-20 left-[188px] top-0 absolute rounded" src="/images/Vector 1.svg"/>
             </div>
             <div class="w-72 h-48 relative">
-                <div class="w-64 h-44 left-0 top-[10px] absolute bg-fuchsia-500 rounded-md"></div>
+                <div class="w-64 h-44 left-0 top-[10px] absolute bg-[#FF41DF] rounded-md"></div>
                 <div class="w-60 h-40 left-[5px] top-[15px] absolute rounded-md border border-fuchsia-800"></div>
-                <div class="w-56 left-[18px] top-[117px] absolute justify-start text-black text-xl font-bold font-['Craftwork_Grotesk'] uppercase">приватних партнерств</div>
-                <div class="left-[18px] top-[25px] absolute justify-start text-black text-6xl font-bold font-['Craftwork_Grotesk'] uppercase">10+</div>
+                <div class="w-56 left-[18px] top-[117px] absolute justify-start text-white text-xl font-bold font-['Craftwork_Grotesk'] uppercase">СЕРЕДНЯ ЗП БАЙЕРА</div>
+                <div class="left-[18px] top-[5px] absolute justify-start text-white text-[55px] font-bold font-['Craftwork_Grotesk'] uppercase">5000$</div>
                 <img class="w-20 h-20 left-[188px] top-0 absolute rounded" src="/images/Vector 5.svg"/>
             </div>
             <div class="w-72 h-48 relative">
-                <div class="w-64 h-44 left-0 top-[10px] absolute bg-fuchsia-500 rounded-md"></div>
+                <div class="w-64 h-44 left-0 top-[10px] absolute bg-[#FF41DF] rounded-md"></div>
                 <div class="w-60 h-40 left-[5px] top-[15px] absolute rounded-md border border-fuchsia-800"></div>
-                <div class="w-56 left-[18px] top-[117px] absolute justify-start text-black text-xl font-bold font-['Craftwork_Grotesk'] uppercase">приватних партнерств</div>
-                <div class="left-[18px] top-[25px] absolute justify-start text-black text-6xl font-bold font-['Craftwork_Grotesk'] uppercase">10+</div>
+                <div class="w-56 left-[18px] top-[117px] absolute justify-start text-white text-xl font-bold font-['Craftwork_Grotesk'] uppercase">НА ПОШУК ОФЕРУ</div>
+                <div class="left-[18px] top-[5px] absolute justify-start text-white text-[25px] font-bold font-['Craftwork_Grotesk'] uppercase"><span class="text-[55px]"><span class="font-[sans]">&lt;</span>12</span> годин</div>
                 <img class="w-20 h-20 left-[188px] top-0 absolute rounded" src="/images/Vector 5.svg"/>
             </div>
             <div class="w-72 h-48 relative">
-                <div class="w-64 h-44 left-0 top-[10px] absolute bg-fuchsia-500 rounded-md"></div>
+                <div class="w-64 h-44 left-0 top-[10px] absolute bg-[#FF41DF] rounded-md"></div>
                 <div class="w-60 h-40 left-[5px] top-[15px] absolute rounded-md border border-fuchsia-800"></div>
-                <div class="w-56 left-[18px] top-[117px] absolute justify-start text-black text-xl font-bold font-['Craftwork_Grotesk'] uppercase">приватних партнерств</div>
-                <div class="left-[18px] top-[25px] absolute justify-start text-black text-6xl font-bold font-['Craftwork_Grotesk'] uppercase">10+</div>
+                <div class="w-56 left-[18px] top-[85px] absolute justify-start text-white text-[20px] font-bold font-['Craftwork_Grotesk'] uppercase">ПАРТНЕРІВ ПО АГЕНТСЬКИМ КАБІНЕТАМ</div>
+                <div class="left-[10px] top-[5px] absolute justify-start text-white text-[55px] font-bold font-['Craftwork_Grotesk'] uppercase">10</div>
                 <img class="w-20 h-20 left-[188px] top-0 absolute rounded" src="/images/Vector 5.svg"/>
             </div>
         </div>
