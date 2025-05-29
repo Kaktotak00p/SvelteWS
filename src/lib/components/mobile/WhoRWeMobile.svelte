47866de822777a01570cf08aad44686451fcd056
Mikhail Kuzin <kaktotak00p@gmail.com>
2025-05-29 18:17:27 +0200

    fixes

--- src/lib/components/mobile/WhoRWeMobile.svelte
+++ src/lib/components/mobile/WhoRWeMobile.svelte
@@ -1,4 +1,4 @@
-<div class="w-[100%] h-[1000px] relative bg-black overflow-hidden mx-auto" id="who">
+<div class="w-[100%] h-[1300px] relative bg-black overflow-hidden mx-auto" id="who">
     <div class=" w-[100%] h-[924px] relative">
         <div class="left-[15px] top-0 absolute text-white text-3xl font-normal font-['Craftwork_Grotesk'] uppercase">//:ХТО МИ</div>
         <div class="w-full px-[15px] relative inline-flex flex-col justify-start items-start gap-10 mt-20">
@@ -23,21 +23,22 @@
             <div class="self-stretch justify-start text-white text-xl font-semibold font-['Craftwork_Grotesk'] uppercase text-justify"> і працювати з командою, <br>яка надихає результатами</div>
         </div>
         <div class="w-[100%] h-96 left-0 top-[533px] absolute">
-            <div class="w-[100%] h-96 left-0 top-0  bg-lime-400">
-                <div class=" w-full inline-flex flex-col justify-start items-center text-nowrap gap-5 mt-12">
-                    <div class="w-48 flex flex-col justify-start items-center">
-                        <div class="self-stretch justify-start text-stone-950 text-4xl font-medium font-['Craftwork_Grotesk']">31 336 372</div>
-                        <div class="justify-start text-stone-950 text-xl font-semibold font-['Craftwork_Grotesk']">FTD</div>
+            <div class="w-[100%] h-96 left-0 top-0 ">
+                <div class=" w-full inline-flex flex-col justify-start items-center text-nowrap gap-16 mt-12 px-12">
+                    <img src="/images/unionCounter.svg" class="absolute w-full px-8">
+                    <div class="w-48 flex flex-col justify-start items-center mt-20">
+                        <div class="self-stretch justify-start text-white text-4xl font-medium font-['Craftwork_Grotesk']">31 336 372</div>
+                        <div class="justify-start text-white text-xl font-semibold font-['Craftwork_Grotesk']">FTD</div>
                     </div>
                     <div class="w-full h-0 rounded outline outline-[3px] outline-offset-[-1.50px] outline-lime-600"></div>
                     <div class="w-36 flex flex-col justify-start items-center">
-                        <div class="self-stretch justify-start text-stone-950 text-4xl font-medium font-['Craftwork_Grotesk']">iGaming</div>
-                        <div class="justify-start text-stone-950 text-xl font-semibold font-['Craftwork_Grotesk']">вертикаль</div>
+                        <div class="self-stretch justify-start text-white text-4xl font-medium font-['Craftwork_Grotesk']">iGaming</div>
+                        <div class="justify-start text-white text-xl font-semibold font-['Craftwork_Grotesk']">вертикаль</div>
                     </div>
                     <div class="w-full h-0 rounded outline outline-[3px] outline-offset-[-1.50px] outline-lime-600"></div>
                     <div class="self-stretch flex flex-col justify-start items-center">
-                        <div class="text-center justify-start text-stone-950 text-4xl font-medium font-['Craftwork_Grotesk']">FB |ASO |IN-APP</div>
-                        <div class="justify-start text-stone-950 text-xl font-semibold font-['Craftwork_Grotesk']">джерела</div>
+                        <div class="text-center justify-start text-white text-4xl font-medium font-['Craftwork_Grotesk']">FB |ASO |IN-APP</div>
+                        <div class="justify-start text-white text-xl font-semibold font-['Craftwork_Grotesk']">джерела</div>
                     </div>
                 </div>
             </div>
