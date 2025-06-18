8e566bb8442f886dd2fd3f7c3f5ec8637d27ac45
Mikle Kuzin <kaktotak00p@gmail.com>
2025-06-18 04:43:39 +0200

    fixes

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import node from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
-const isVercel = process.env.Vercel === '1' || process.env.VERCEL === 'true';
+const prod = process.env.DEPLOY_TARGET === 'node';
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
@@ -13,7 +13,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: isVercel ? vercel() : node({}),
+    adapter: prod ? node({}) : vercel() ,
 	}
 };
 
